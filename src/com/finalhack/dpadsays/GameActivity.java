c223573e48b73c82c052ef5876188123f63429b8
shellum <cameron.shellum@octanner.com>
2013-01-03 19:37:41 -0700

    initial commit

---
+++ src/com/finalhack/dpadsays/GameActivity.java
@@ -0,0 +1,24 @@
+package com.finalhack.dpadsays;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_game);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_game, menu);
+		return true;
+	}
+
+}
